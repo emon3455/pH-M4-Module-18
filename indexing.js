a97c9480ee6ec395475c81d538aadce4229a6ca0
emon3455 <emon.mhk69@gmail.com>
2023-01-26 22:12:23 +0600

    get, set element by index and indexOf method

---
+++ indexing.js
@@ -0,0 +1,13 @@
+var age = [21 , 22 , 21 , 17];
+
+// get element value from index:
+var element = age[2];
+console.log(element);
+
+//Set element value by index:
+age[0]=23;
+console.log(age);
+
+// find index of an element:
+var indexOf17 = age.indexOf(17);
+console.log(indexOf17);
