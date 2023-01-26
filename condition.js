f64e31e176ecaf0b47c16be827f2aedbb0c349b1
emon3455 <emon.mhk69@gmail.com>
2023-01-26 23:08:25 +0600

    basic conditional operations

---
+++ condition.js
@@ -0,0 +1,12 @@
+// basic conditional operations:
+
+var productPrice = 35000;
+var myMoney = 150000;
+
+if(productPrice <= myMoney){
+    console.log("Hurray!! , i can but the Product")
+}
+else{
+    console.log("Oh!! No , i cannot buy the product");
+}
+
