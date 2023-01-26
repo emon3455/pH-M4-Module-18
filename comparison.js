e19e8c22b7092df760cf8df273f2f67cce858a02
emon3455 <emon.mhk69@gmail.com>
2023-01-26 22:56:03 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,12 @@
+var x =12;
+var y =15;
+var z =15;
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x === y);
+console.log(x !== y);
+
+console.log(x < y && y==z);
+console.log(x>y || y==z);
