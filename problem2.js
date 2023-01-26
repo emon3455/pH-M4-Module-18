32372ff6bb1df06d7d27c1d0eff3c9dc9d593328
emon3455 <emon.mhk69@gmail.com>
2023-01-27 00:14:08 +0600

    problem 2 solved

---
+++ problem2.js
@@ -0,0 +1,20 @@
+var marks = 40;
+
+if(marks >= 80){
+    console.log("A");
+}
+else if (marks >= 60){
+    console.log("B");
+}
+
+else if (marks >= 50){
+    console.log("C");
+}
+
+else if(marks>=40){
+    console.log("D");
+}
+
+else{
+    console.log("Fail");
+}
