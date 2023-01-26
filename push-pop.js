a0e7f0740f5abe8fb24469e96cbb8fdab8646b1b
emon3455 <emon.mhk69@gmail.com>
2023-01-26 22:39:58 +0600

    push and pop

---
+++ push-pop.js
@@ -0,0 +1,13 @@
+//push and pop:
+
+var student = ["Emon","Aminul","Rafi","Akhter"];
+
+student.push("Elina");
+student.push("Tuspa");
+console.log(student);
+
+
+student.pop();
+var element = student.pop();
+console.log(student);
+console.log(element);
